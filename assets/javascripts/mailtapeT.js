6cd2505f9ed1f21c3606e20516f59961283db8b6
Evryjazz <julienzanni@gmail.com>
2016-01-31 20:53:46 +0100

    Universal Analytics Implementation

--- assets/javascripts/mailtapeT.js
+++ assets/javascripts/mailtapeT.js
@@ -287,9 +287,6 @@ $(document).ready(
 		    	var prenom = $( "#mc-PRENOM" ).val();
 		        $('#mc-form').fadeOut('fast', function() {
 		        	$('#footerSubscription-form-text').html("Thank's "+prenom+", you're gonna love Sunday morning. We've just sent you a confirmation email !");
-		        	//_gaq.push(['_trackEvent', 'CTA Episode', 'Subscribe', 'Submitted info']);
-		        	ga('send', 'event', 'Email Subscription', 'Subscription on Episode {{ page.category }} - {{ page.guest_name }}', 'Yeah!!!');
-		        	//try {mixpanel.track("EP > Top Bar Subscription > Step 2, Subscribed");} catch(e) {}
 		        });
 		        setTimeout(function() {
 	    			$('#footerSubscription').removeClass("hidden-xs").fadeOut('slow');
@@ -312,15 +309,9 @@ $(document).ready(
 		        	if (isFromEmail()) {
 		        		$('#mc-sidebar h1').html("You're beautiful !");
 		        		$('#mc-sidebar h2').html(prenom+" will receive a confirmation email, be sure to let your dear friend know it's coming from you !");
-		        		//_gaq.push(['_trackEvent', 'CTA Episode', 'Subscribe', 'Subscribed a friend']);
-		        		ga('send', 'event', 'Email Subscription', 'Friend Subscription on Episode {{ page.category }} - {{ page.guest_name }}', 'Yeah!!!');
-		        		//try{mixpanel.track("EP > Sidebar Friends Subscription > Subscribed a friend");} catch(e) {}
 		        	} else {
 			        	$('#mc-sidebar h1').html("Yo "+prenom+" !");
 			        	$('#mc-sidebar h2').html("Thanks, we've just sent you a confirmation email ;)");
-			        	//_gaq.push(['_trackEvent', 'CTA Episode', 'Subscribe', 'Subscribed from Sidebar']);
-			        	ga('send', 'event', 'Email Subscription', 'Subscription on Episode {{ page.category }} - {{ page.guest_name }}', 'Yeah!!!');
-			        	//try {mixpanel.track("EP > Sidebar Subscription > Subscribed from sidebar");} catch(e) {};
 			        }
 		        });
 		         setTimeout(function() {
